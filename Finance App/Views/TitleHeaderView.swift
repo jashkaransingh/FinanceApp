f23ebae5195522942d22f2d1e971cb71c6b67ec6
jashkaransingh <jvs7350@psu.edu>
2025-05-29 23:31:47 -0400

    Added custom Nav title. Added tap animations in cards.

---
+++ Finance App/Views/TitleHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  TitleHeaderView.swift
+//  Finance App
+//
+//  Created by Jas  on 5/29/25.
+//
+
+import UIKit
+
+class TitleHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
