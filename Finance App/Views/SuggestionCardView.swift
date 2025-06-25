6f30f0b2b4150e44be0d81b98c7545a573026e63
jashkaransingh <jvs7350@psu.edu>
2025-06-24 23:32:13 -0400

    Removed the old UI for Budget AI. Connected the new UI for budget Ai with the backend. Also, fixed bug in the budget AI

---
+++ Finance App/Views/SuggestionCardView.swift
@@ -0,0 +1,20 @@
+//
+//  SuggestionCardView.swift
+//  Finance App
+//
+//  Created by Jas  on 6/21/25.
+//
+
+import UIKit
+
+class SuggestionCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Finance App/Views/WalletCardView.swift
@@ -0,0 +1,20 @@
+//
+//  WalletCardView.swift
+//  Finance App
+//
+//  Created by Jas  on 6/21/25.
+//
+
+import UIKit
+
+class WalletCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
